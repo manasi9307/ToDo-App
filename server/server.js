9397ae62eec1e51ef2519b45e4c2fa071eb37224
manasi9307 <manasi.thakur@sjsu.edu>
2018-07-16 16:41:09 -0700

    Login functionality

--- server/server.js
+++ server/server.js
@@ -102,12 +102,23 @@ app.post('/users', (req, res) => {
 
 
 
-app.get('/users/me',authenticate, (req, res) => {
+app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
 app.listen(port, () => {
   console.log(`Starting server on ${port}`);
+});
+
+app.post('/users/login',(req, res) => {
+  var body = _.pick(req.body,['email','password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+   return user.generateAuthToken().then((token) => {
+     res.header('x-auth', token).send(user);
+   });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 module.exports = {app};

60f3a51329482f912cbc6b16a4dc31b79d5b0ff4
manasi9307 <manasi.thakur@sjsu.edu>
2018-07-16 14:03:41 -0700

    hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id:10
-}
+var password = 'abc';
 
-var token = jwt.sign(data, '123abc');
-//jwt.verify()
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedpassword = '2a$10$0zlsICzjiVbZeBPnoiPnaOa7WzUt5y/cCkdsCkhkS/UnA3PXO83Kq';
+
+bcrypt.compare(password, hashedpassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id:10
+// }
+
+// var token = jwt.sign(data, '123abc');
+// //jwt.verify()
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
